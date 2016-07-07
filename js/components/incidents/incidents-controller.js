54879c2847429d6f7d3a5518a91e0b8589c3f842
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2016-07-07 20:26:19 +0200

    no message

--- js/components/incidents/incidents-controller.js
+++ js/components/incidents/incidents-controller.js
@@ -2,4 +2,25 @@ app.controller('IncidentsController', ['$scope', '$http', function($scope, $http
 
 	$("header nav").attr("class", "incidents");
 
+	var apiUrl = 'api/Get_IncidencesResult.json';
+
+    $scope.data = {};
+
+    $scope.loadData = function()
+    {
+        $http({
+            method  : 'GET',
+            url     : apiUrl
+         })
+        .success(function(data) {
+        	// parse date: var date = new Date(parseInt(jsonDate.substr(6)));
+        	// console.log(data);
+            if (data) {
+                $scope.data = data.Get_IncidencesResult;
+            }
+        });
+    }
+
+    $scope.loadData();
+
 }]);
