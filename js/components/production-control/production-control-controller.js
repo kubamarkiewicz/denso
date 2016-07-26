3745bcb869da94abec5739d49baea1fa49d38799
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2016-07-26 22:23:31 +0200

    no message

---
+++ js/components/production-control/production-control-controller.js
@@ -0,0 +1,60 @@
+app.controller('ProductionControlController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) { 
+
+	$("header nav").attr("class", "production-control");
+
+    $scope.ordersData = {};
+    $scope.destinationsData = {};
+    
+    $rootScope.clearIntervals();
+
+
+    $scope.loadOrdersData = function()
+    {
+        $http({
+            method  : 'GET',
+            url     : $rootScope.settings.webServiceURLs.getOrders
+         })
+        .success(function(data) {
+            if (data) {
+                $scope.ordersData = data.Get_OrdersResult;
+            }
+        });
+    }
+    $scope.loadOrdersData();
+    $rootScope.setInterval(function(){
+        if ($('.production-control select:focus').length==0) {
+            $scope.loadOrdersData();
+        };
+    });
+
+
+
+    $scope.loadDestinationsData = function()
+    {
+        $http({
+            method  : 'GET',
+            url     : $rootScope.settings.webServiceURLs.getDestinations
+         })
+        .success(function(data) {
+            if (data) {
+                $scope.destinationsData = data.Get_Order_DestinationsResult;
+            }
+        });
+    }
+    $scope.loadDestinationsData();
+
+
+    $scope.setOrder = function(item)
+    {
+        $http({
+            method  : 'GET',
+            url     : $rootScope.settings.webServiceURLs.setOrder,
+            params  : {Time: item.Time, IdDestination: item.IdDestination}
+         })
+        .success(function(data) {
+            $scope.loadOrdersData();
+        });
+    }
+
+
+}]);
